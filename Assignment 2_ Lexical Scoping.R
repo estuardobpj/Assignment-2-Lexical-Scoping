e9acdb024b829268f35535888189c0377f16f0d8
Juan Estuardo Bocel Pocop <68825394+estuardobpj@users.noreply.github.com>
2020-09-06 15:52:19 -0600

    Add files via upload

---
+++ Assignment 2_ Lexical Scoping.R
@@ -0,0 +1,34 @@
+
+
+    ###Caching the Mean of a Vector###
+
+#setwd() #this one is for setting the directory
+
+makeVector <- function(x = matrix(sample(1:100,9),3,3)) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+###Caching the Inverse of a Matrix###
+
+
+cacheSolvemx <- function(x, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
+}
